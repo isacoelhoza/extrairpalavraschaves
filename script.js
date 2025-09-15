0368bff55fed6b98e9511519b995e1b71ddaf200
isacoelhoza <isabelle.zanatta@escola.pr.gov.br>
2025-09-15 07:59:50 -0300

    15/09

--- script.js
+++ script.js
@@ -8,5 +8,23 @@ function () {
     const texto = document.querySelector ('entrada-de-texto').value;
     const campoResultado = document.querySelector (#resultado-PalavrasChave);
     const palavrasChave = processa.Texto (texto);
+
+    campoResultado.textContent = palavrasChave,join(",");
 }
-campoResultado.textContent = palavrasChave,join(",");
+
+function processaTexto(texto){
+let palavras = texto.split(/\P{L}+/u);
+
+for(let i in palavras){
+palavras (i) = palavras(i).toLowerCase();
+}
+
+palavras = tiraPalavrasRuins(palavras)
+
+const frequencias = contaFrequencias(palavras);
+let ordenadas = Object.keys(frequencias).sort(ordenapalavra);
+
+function ordenaPalavra(p1,p2){
+    return frequencias(p2) - frequencias(p1)
+}
+}
